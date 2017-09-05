4dcf458efa352221abf90ccca6812afe5bd65f19
Lakshmi Narayana Mekala <lmekala@tibco.com>
2017-09-05 12:47:00 +0530

    Updating

--- samples-recipes/build-c-new.sh
+++ samples-recipes/build-c-new.sh
@@ -82,11 +82,11 @@
 									rm  -r "${Gateway[$i]}-${TRAVIS_OS_NAME}" ;
 								fi
 						# Exit if directory not found
-						else
-								echo "failed to create ${Gateway[$i]}-${TRAVIS_OS_NAME} gateway" 
-								echo "directory ${Gateway[$i]}-${TRAVIS_OS_NAME}" not found
-								exit 1
-						fi
+						#else
+						#		echo "failed to create ${Gateway[$i]}-${TRAVIS_OS_NAME} gateway" 
+						#		echo "directory ${Gateway[$i]}-${TRAVIS_OS_NAME}" not found
+						#		exit 1
+						#fi
 				done
 				cd .. ;
 	}
