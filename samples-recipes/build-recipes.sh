20b4c4fa4b563f77018556389a5c2926f2eb12b5
Lakshmi Narayana Mekala <lmekala@tibco.com>
2017-09-09 09:31:01 +0530

    Update 1

--- samples-recipes/build-recipes.sh
+++ samples-recipes/build-recipes.sh
@@ -34,8 +34,8 @@
 	{
 		echo "Creating gateway"
 		#Gateway=({"envoy-invoker-mashling","inline-gateway"})
-        #Gateway=({"envoy-invoker-mashling","inline-gateway","kafka-conditional-gateway","kafka-reference-gateway","KafkaTrigger-To-KafkaActivity-mashling","KafkaTrigger-To-RestActivity-mashling","reference-gateway","RestTrigger-To-KafkaActivity-mashling","rest-conditional-gateway"})		
-		Gateway=({"envoy-invoker-mashling","reference-gateway"})		
+        Gateway=({"envoy-invoker-mashling","inline-gateway","kafka-conditional-gateway","kafka-reference-gateway","KafkaTrigger-To-KafkaActivity-mashling","KafkaTrigger-To-RestActivity-mashling","reference-gateway","RestTrigger-To-KafkaActivity-mashling","rest-conditional-gateway"})		
+		#Gateway=({"envoy-invoker-mashling","reference-gateway"})		
 			# get length of an array		
 			tLen="${#Gateway[@]}"
 				for (( i=0; i<"${tLen}"; i++ ));
@@ -100,13 +100,15 @@
 	else
 		multi_os ;	
 	fi
+
+	cd .. ;
    # Calling function to create recipes binaries 
   #  multi_os ;
 
     git config user.email "lmekala@tibco.com";
 	git config user.name "LakshmiMekala"
 	
-	cd .. ;
+	
 	ls ;
 	pwd ;
 	git add .;  
