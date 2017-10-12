f229e04a0ac8dca65db4a9c4a054eed21d56a8bf
Lakshmi Narayana Mekala <lmekala@tibco.com>
2017-10-12 14:41:31 +0530

    V1.2

--- samples-recipes/f19.sh
+++ samples-recipes/f19.sh
@@ -51,9 +51,9 @@
                 #    publish=$(cat $GOPATH/src/github.com/TIBCOSoftware/mashling-recipes/recipe_registry.json | jq $xpath_publish) ;
                     #echo "$publish";
                     #publish_gateway ;
-            publish_length=$(cat $GOPATH/src/github.com/TIBCOSoftware/mashling-recipes/recipe_registry.json | jq '$xpath_publish | length') ; 
+            publish_length=$(cat $GOPATH/src/github.com/TIBCOSoftware/mashling-recipes/recipe_registry.json | jq $xpath_publish' | length') ; 
 		    echo "Found $publish_length recipes." ;        
-		        for (( x = 0; x < $publish_length ; x++ ))
+		        for (( x=0; x<$publish_length; x++ ))
                 do  
                     eval xpath_recipe='.recipe_repos[$j].publish[$x].recipe' ;
                     Gateway[$x]=$(cat $GOPATH/src/github.com/TIBCOSoftware/mashling-recipes/recipe_registry.json | jq $xpath_recipe) ;
