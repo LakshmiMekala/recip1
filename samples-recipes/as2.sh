bf8c87db44025919f66a173a510368d18dfa735f
Lakshmi Narayana Mekala <lmekala@tibco.com>
2017-10-30 12:34:25 +0530

    v1

--- samples-recipes/as2.sh
+++ samples-recipes/as2.sh
@@ -2,17 +2,17 @@
 
 mkdir ${HOME}/.aws
 
-cat > ${HOME}/.aws/credentials << EOL
+cat > ${HOME}/.aws/credentials <<EOL
 [default]
 access_key_id = ${SITE_KEY}
 secret_access_key = ${SITE_KEY_SECRET}
-EOL 
+EOL
 
-cat > ${HOME}/.aws/config << EOL
+cat > ${HOME}/.aws/config <<EOL
 [default]
 output = json
 region = ${AWS_REGION}
-EOL 
+EOL
 
 aws s3 rm s3://test-bucket4569/master-builds/latest --recursive
 
