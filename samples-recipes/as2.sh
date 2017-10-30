78268faad692a0584f0a9c2bb323e1d95af8a2c8
Lakshmi Narayana Mekala <lmekala@tibco.com>
2017-10-30 12:55:25 +0530

    check build

--- samples-recipes/as2.sh
+++ samples-recipes/as2.sh
@@ -8,11 +8,11 @@ aws_access_key_id = ${SITE_KEY}
 aws_secret_access_key = ${SITE_KEY_SECRET}
 EOL
 
-cat > ${HOME}/.aws/config <<EOL
-[default]
-output = json
-region = ${AWS_REGION}
-EOL
+#cat > ${HOME}/.aws/config <<EOL
+#[default]
+#output = json
+#region = ${AWS_REGION}
+#EOL
 
 aws s3 rm s3://test-bucket4569/master-builds/latest --recursive
 
