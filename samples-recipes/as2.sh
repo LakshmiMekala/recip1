dc52a1a61367f23e722d7cedc664e76c7308415a
LakshmiMekala <lmekala@tibco.com>
2017-10-30 19:08:05 +0530

    v2 (#2)
    * v2
    
    * v9

--- samples-recipes/as2.sh
+++ samples-recipes/as2.sh
@@ -8,7 +8,9 @@ aws_access_key_id = ${SITE_KEY}
 aws_secret_access_key = ${SITE_KEY_SECRET}
 EOL
 
+echo "test123"
     if [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then    
+        echo "cleaning S3 folder"
         aws s3 rm s3://test-bucket4569/master-builds/latest --recursive
     fi
 
