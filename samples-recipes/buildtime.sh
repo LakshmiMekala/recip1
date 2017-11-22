44183db0aa7e51557e21355bda75d8584bc3e5d3
Lakshmi Narayana Mekala <lmekala@tibco.com>
2017-11-22 15:06:03 +0530

    fourth commit

--- samples-recipes/buildtime.sh
+++ samples-recipes/buildtime.sh
@@ -61,12 +61,12 @@
                 do  
                     eval xpath_recipe='.recipe_repos[$j].publish[$x].recipe' ;
                     Gateway[$x]=$(cat $GOPATH/src/github.com/TIBCOSoftware/mashling-recipes/recipe_registry.json | jq $xpath_recipe) ;
-
                     Gateway[$x]=$(echo ${Gateway[$x]} | tr -d '"') ;
                     y=0;
-                    if [[ $recipeName =~ ($Gateway[$x]/manifest | $Gateway[$x]/$Gateway[$x].json) ]] ;then
+                    if [[ $recipeName =~ $Gateway[$x]/$Gateway[$x].json ]] || [[ $recipeName =~ $Gateway[$x]/manifest ]];then
                         echo "$Gateway[$x] found in current commit" ;
                         recipeCreate[$y]=$Gateway[$x] ;
+                        echo "$recipeCreate[$y]" ;
                         y=y+1 ;
                     else
                         echo "$Gateway[$x] not found in current commit ";
