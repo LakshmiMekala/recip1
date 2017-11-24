3633e5637a91a9e5b871ca01b6e55e2a3862f7ee
Lakshmi Narayana Mekala <lmekala@tibco.com>
2017-11-24 13:07:12 +0530

    timestamp file update

--- samples-recipes/final.sh
+++ samples-recipes/final.sh
@@ -126,6 +126,7 @@ echo "==========================================================================
                         Gateway[$x]=$(cat $GOPATH/src/github.com/TIBCOSoftware/mashling-recipes/recipe_registry.json | jq $xpath_recipe) ;
                         Gateway[$x]=$(echo ${Gateway[$x]} | tr -d '"') ;      
                     done
+                    echo "alertA" ;
                     deleteLatest ;   
                 fi
 echo "=========================================================================="
