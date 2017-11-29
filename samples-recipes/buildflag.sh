84d70c2c305b5aea9fbd32e9a7a4a9cde1301db1
Lakshmi Narayana Mekala <lmekala@tibco.com>
2017-11-29 11:28:06 +0530

    recipe info.json check for full build

--- samples-recipes/buildflag.sh
+++ samples-recipes/buildflag.sh
@@ -131,7 +131,7 @@ function recipe_registry()
                 fi
                 recipeCreate=()
                 y=0;
-                for (( x=2; x<$publish_length; x++ ))
+                for (( x=0; x<$publish_length; x++ ))
                 do
                     eval xpath_recipe='.recipe_repos[$j].publish[$x].recipe' ;
                     Gateway[$x]=$(cat $GOPATH/src/github.com/TIBCOSoftware/mashling-recipes/recipe_registry.json | jq $xpath_recipe) ;
@@ -161,12 +161,12 @@ function recipe_registry()
                     else
                         echo "recipe needs to be created from full build"
                         y=$y+1;
-                        #recipeInfo ;
                     fi
+                    recipeInfo ;
                 done
                 echo "${recipeCreate[@]}" ;
                 echo #################################
-                RecipesToBeCreated ;
+                #RecipesToBeCreated ;
             done
 }
 
