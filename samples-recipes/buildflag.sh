874c40254231f664b2706b17bc20170d4bcd70b4
Lakshmi Narayana Mekala <lmekala@tibco.com>
2017-11-29 12:49:57 +0530

    optimizing build-500

--- samples-recipes/buildflag.sh
+++ samples-recipes/buildflag.sh
@@ -136,7 +136,7 @@ function recipe_registry()
                     eval xpath_recipe='.recipe_repos[$j].publish[$x].recipe' ;
                     Gateway[$x]=$(cat $GOPATH/src/github.com/TIBCOSoftware/mashling-recipes/recipe_registry.json | jq $xpath_recipe) ;
                     Gateway[$x]=$(echo ${Gateway[$x]} | tr -d '"') ;
-                    recipeCreate[$y]=${Gateway[$x]} ;
+                    #recipeCreate[$y]=${Gateway[$x]} ;
                     echo #################################
                     echo "${Gateway[$x]}" ;
                     echo "${recipeCreate[$y]}";
@@ -169,7 +169,7 @@ function recipe_registry()
                 done
                 echo "${recipeCreate[@]}" ;
                 echo #################################
-                #RecipesToBeCreated ;
+                RecipesToBeCreated ;
             done
 }
 
@@ -333,7 +333,7 @@ function recipeInfo()
         popd ;
 
 
-        # cd $GOPATH/src/github.com/TIBCOSoftware/recip1/samples-recipes/master-builds
-        # git add .
-        # git commit -m "uploading v1"
-        # git push
+        cd $GOPATH/src/github.com/TIBCOSoftware/recip1/samples-recipes/master-builds
+        git add .
+        git commit -m "uploading v1"
+        git push
