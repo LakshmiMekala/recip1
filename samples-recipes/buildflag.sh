bcb7034d591cc3e58dd4e046eb20b80d5c523af7
Lakshmi Narayana Mekala <lmekala@tibco.com>
2017-12-01 15:06:09 +0530

    creating 2 recipes

--- samples-recipes/buildflag.sh
+++ samples-recipes/buildflag.sh
@@ -154,7 +154,7 @@ function recipe_registry()
                 fi
                 recipeCreate=()
                 y=0;
-                for (( x=2; x<$publish_length; x++ ))
+                for (( x=0; x<$publish_length; x++ ))
                 do
                     eval xpath_recipe='.recipe_repos[$j].publish[$x].recipe' ;
                     Gateway[$x]=$(cat $GOPATH/src/github.com/TIBCOSoftware/mashling-recipes/recipe_registry.json | jq $xpath_recipe) ;
